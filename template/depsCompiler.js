c0612e48907b31cfec88b5611482263de07c542a
Gaël Demette <demette.gael@gmail.com>
2015-01-20 14:06:15 +0100

    Simplified

--- template/depsCompiler.js
+++ template/depsCompiler.js
@@ -1,32 +1,18 @@
 "use strict";
-var less = require('less');
 var path = require('path');
 var fs = require('fs');
 
-var lessPath = path.resolve(path.join(__dirname, '..', 'node_modules', 'bootstrap', 'less'));
-var file = path.resolve(path.join(lessPath, 'bootstrap.less'));
-var parser = new(less.Parser)({
-  paths: [lessPath],
-  filename: 'bootstrap.less'
-});
-
-
 module.exports = function(services) {
   var bootstrapTemplate = services.findById('apolloTemplates').findById('bootstrap');
-  var less = require('less');
 
-  fs.readFile(file, function (err, less) {
-    if (err) throw err;
-    parser.parse(less.toString('utf8'), function (e, tree) {
-      var css = tree.toCSS({
-        compress: true
-      });
-      var outputCss = path.resolve(path.join(bootstrapTemplate.path, 'public', 'css', 'bootstrap.css'));
-      fs.writeFile(outputCss, css, function (err) {
-        if (err) throw err;
-        console.log('Bootstrap.css generated');
-      });
-    });
+  var bootstrapFromPath = path.resolve(path.join(__dirname, '..', 'node_modules', 'bootstrap', 'dist'));
+  var jqueryFromPath = path.resolve(path.join(__dirname, '..', 'node_modules', 'jquery', 'dist'));
+  var bootstrapToPath = path.resolve(path.join(bootstrapTemplate.path, 'public', 'bootstrap'));
+  var jqueryToPath = path.resolve(path.join(bootstrapTemplate.path, 'public', 'jquery'));
+  fs.unlink(bootstrapToPath, function() {
+    fs.symlinkSync(bootstrapFromPath, bootstrapToPath, 'dir');
   });
-
+  fs.unlink(jqueryToPath, function() {
+    fs.symlinkSync(jqueryFromPath, jqueryToPath, 'dir');
+  });
 };
